f2578c3b92163cf186622de6aa24303984b567e1
chanwoong1 <jcw7468@gmail.com>
2025-04-10 13:45:25 +0900

    feature: 책 검색 결과 컴포넌트 추가
    검색 결과 시 표시될 컴포넌트를 추가하였습니다.

---
+++ packages/ui/src/book/search-result/data.ts
@@ -0,0 +1,42 @@
+import { BookSearchResultProps } from "./default";
+
+const dummyBookSearchResults: BookSearchResultProps[] = [
+  {
+    id: 1,
+    isbn: 9788931476804,
+    bookmark: false,
+    image: "https://image.yes24.com/goods/139798289/XL",
+    title: "2025 이기적 라눅스마스터 1급 (1·2차)",
+    author: "권소라",
+    publisher: "영진닷컴",
+  },
+  {
+    id: 2,
+    isbn: 9788931476804,
+    bookmark: true,
+    image: "https://image.yes24.com/goods/139798289/XL",
+    title: "2025 이기적 라눅스마스터 1급 (1·2차)",
+    author: "권소라",
+    publisher: "영진닷컴",
+  },
+  {
+    id: 3,
+    isbn: 9788931476804,
+    bookmark: false,
+    image: "https://image.yes24.com/goods/139798289/XL",
+    title: "2025 이기적 라눅스마스터 1급 (1·2차)",
+    author: "권소라",
+    publisher: "영진닷컴",
+  },
+  {
+    id: 4,
+    isbn: 9788931476804,
+    bookmark: true,
+    image: "https://image.yes24.com/goods/139798289/XL",
+    title: "2025 이기적 라눅스마스터 1급 (1·2차)",
+    author: "권소라",
+    publisher: "영진닷컴",
+  },
+];
+
+export default dummyBookSearchResults;
