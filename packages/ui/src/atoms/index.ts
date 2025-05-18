91fc10be1217b45b716685f46d1be3d579529107
dokimion24 <leecy3610@gmail.com>
2025-05-19 07:45:18 +0900

    feature: export Label and Select components

--- packages/ui/src/atoms/index.ts
+++ packages/ui/src/atoms/index.ts
@@ -1,5 +1,7 @@
 export * from "./button";
 export * from "./divider";
 export * from "./form";
+export * from "./label";
+export * from "./select";
 export * from "./text";
 export * from "./textbox";
