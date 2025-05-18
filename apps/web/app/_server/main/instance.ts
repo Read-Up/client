7303ea0314109da0d353017d108fb7ad1bec3284
dokimion24 <leecy3610@gmail.com>
2025-05-19 07:44:39 +0900

    feature: update import path

--- apps/web/app/_server/main/instance.ts
+++ apps/web/app/_server/main/instance.ts
@@ -1,8 +1,8 @@
 import ky from "ky";
 // package
 
-import { apiLogger, serverLogger, HttpError } from "@/_lib";
-import END_POINT from "@/_constant/end-point";
+import { END_POINT } from "@/_constant/end-point";
+import { apiLogger, HttpError, serverLogger } from "@/_lib";
 // import { getServerToken } from './helper';
 
 // ! direct backend api instance
