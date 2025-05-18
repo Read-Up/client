203de8ba6ea8d9d1ac9ec738fbf25d088c43de40
dokimion24 <leecy3610@gmail.com>
2025-05-19 07:33:58 +0900

    feature: implement dynamic quiz creation with add button in QuizCreateList component

--- apps/web/app/quiz/new/_components/quiz-create-list.tsx
+++ apps/web/app/quiz/new/_components/quiz-create-list.tsx
@@ -1,11 +1,40 @@
 "use client";
 
+import { Button } from "@readup/ui/atoms";
+import { useFieldArray, useFormContext } from "react-hook-form";
+import { QuizCreateFormData } from "../_types/quiz-create";
 import QuizCreateCard from "./quiz-create-card";
 
 export default function QuizCreateList() {
+  const { control } = useFormContext<QuizCreateFormData>();
+  const { fields, append } = useFieldArray({
+    control,
+    name: "quizRequestList",
+  });
+
+  const handleAddQuiz = () => {
+    append({
+      question: "",
+      explanation: "",
+      quizOptionRequestList: [
+        { content: "", isCorrect: false },
+        { content: "", isCorrect: false },
+      ],
+    });
+  };
+
   return (
     <div className="flex flex-col gap-4">
-      <QuizCreateCard />
+      <div className="flex flex-col gap-4">
+        {fields.map((field, index) => (
+          <QuizCreateCard key={field.id} index={index + 1} />
+        ))}
+      </div>
+      <div className="fw-[98px] mx-auto">
+        <Button variant="outline" onClick={handleAddQuiz}>
+          퀴즈 추가하기
+        </Button>
+      </div>
     </div>
   );
 }
