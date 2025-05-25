e1c38168361e3b159b2659ed0c43be4a756afd21
dokimion24 <leecy3610@gmail.com>
2025-05-25 20:46:24 +0900

    feature: add QuizFormHeader component for quiz creation interface

--- apps/web/app/quiz/new/_components/quiz-form-header.tsx
+++ apps/web/app/quiz/new/_components/quiz-form-header.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { BackSVG } from "@readup/icons";
-import QuizSaveButton from "./quiz-save-button";
+import QuizFormSaveButton from "./quiz-form-save-button";
 
-export default function QuizNewHeader() {
+export default function QuizFormHeader() {
   return (
     <header className="flex justify-between items-center px-[15px] py-[11px]">
       <button className="flex items-center">
         <BackSVG />
       </button>
       <h1 className="typo-title1">퀴즈 작성하기</h1>
-      <QuizSaveButton />
+      <QuizFormSaveButton />
     </header>
   );
 }
