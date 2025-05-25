be4baeaa9c5377ca0f311cdf2849c12291b67eb3
dokimion24 <leecy3610@gmail.com>
2025-05-25 20:47:22 +0900

    feature: update quiz form components to use QuizFormChapterSelect and QuizFormList for improved structure

--- apps/web/app/quiz/new/_components/quiz-form-content.tsx
+++ apps/web/app/quiz/new/_components/quiz-form-content.tsx
@@ -1,16 +1,16 @@
-import { Form } from "@readup/ui/atoms";
+import { Form } from "@readup/ui/atoms/form";
 import { useFormContext } from "react-hook-form";
-import ChapterSelect from "./chapter-select";
-import QuizCreateList from "./quiz-create-list";
+import QuizFormChapterSelect from "./quiz-form-chapter-select";
+import QuizFormList from "./quiz-form-list";
 
 export default function QuizFormContent() {
   const form = useFormContext();
 
   return (
     <Form {...form}>
-      <ChapterSelect />
+      <QuizFormChapterSelect />
       <div className="flex flex-col gap-6">
-        <QuizCreateList />
+        <QuizFormList />
       </div>
     </Form>
   );
