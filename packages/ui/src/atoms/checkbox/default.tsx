bafa72648c4102e92bb31a1b0bb985920f957cc4
dokimion24 <leecy3610@gmail.com>
2025-05-25 20:48:28 +0900

    feature: enhance Checkbox component with size variants and improved styling

--- packages/ui/src/atoms/checkbox/default.tsx
+++ packages/ui/src/atoms/checkbox/default.tsx
@@ -1,27 +1,65 @@
 "use client";
 
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-
-import * as React from "react";
-
 import { CheckedSVG } from "@readup/icons";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
 import { cn } from "../../lib";
 
-function Checkbox({ className, ...props }: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
-  return (
-    <CheckboxPrimitive.Root
-      data-slot="checkbox"
-      className={cn("border-1 border-solid border-primary_variant w-5 h-5 rounded-full relative", className)}
-      {...props}
-    >
-      <CheckboxPrimitive.Indicator
-        data-slot="checkbox-indicator"
-        className="flex items-center justify-center absolute inset-0 text-current"
-      >
-        <CheckedSVG fill="#4A90E2" />
-      </CheckboxPrimitive.Indicator>
-    </CheckboxPrimitive.Root>
-  );
+const checkboxVariants = cva("flex items-center justify-center rounded-full cursor-pointer transition-colors", {
+  variants: {
+    size: {
+      xs: "w-3 h-3",
+      sm: "w-4 h-4",
+      md: "w-5 h-5",
+      lg: "w-6 h-6",
+      xl: "w-8 h-8",
+      "2xl": "w-12 h-12",
+      "3xl": "w-16 h-16",
+    },
+    checked: {
+      true: "",
+      false: "",
+    },
+  },
+  compoundVariants: [
+    {
+      checked: true,
+      className: "bg-primary  border-none",
+    },
+    {
+      checked: false,
+      className: "bg-transparent border border-white",
+    },
+  ],
+  defaultVariants: {
+    size: "md",
+    checked: false,
+  },
+});
+
+export interface CheckboxProps
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, "onChange">,
+    VariantProps<typeof checkboxVariants> {
+  color?: string;
+  size?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl";
+  className?: string;
+  checked?: boolean;
+  onChange?: (checked: boolean) => void;
 }
 
-export { Checkbox };
+export const Checkbox = React.forwardRef<HTMLDivElement, CheckboxProps>(
+  ({ checked = false, size, color = "#4A90E2", className, onChange, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(checkboxVariants({ size, checked }), className)}
+        onClick={() => onChange?.(!checked)}
+        {...props}
+      >
+        {checked && <CheckedSVG size={size} className="overflow-visible cursor-pointer" fill={color} />}
+      </div>
+    );
+  },
+);
+
+Checkbox.displayName = "Checkbox";
