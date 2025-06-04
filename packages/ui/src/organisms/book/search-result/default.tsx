d0b699d707d84063f3dac3f1e0a4984fda4abb39
chanwoong1 <jcw7468@gmail.com>
2025-06-05 08:42:52 +0900

    refactor: ui 패키지 리팩토링
    - modal, drawer, search-result, textbox에 대해 필요한 요소 축 ㅏ및 코드
      리팩토링
    - icon 추가

--- packages/ui/src/organisms/book/search-result/default.tsx
+++ packages/ui/src/organisms/book/search-result/default.tsx
@@ -5,10 +5,13 @@ import React from "react";
 export interface BookSearchResultProps {
   id: number;
   image: string;
-  bookmark: boolean;
+  bookmark?: boolean;
   title: string;
   author: string;
   publisher: string;
+
+  isbn?: string | number; // Optional ISBN for linking to book details
+  showBookmark?: boolean; // Optional prop to control bookmark visibility
 
   onBookmarkToggle?: (id: number, nextValue: boolean) => void;
   onClickDetail?: (id: number) => void;
@@ -27,18 +30,21 @@ function BookSearchResult({
   bookmark,
   onBookmarkToggle,
   onClickDetail,
+  isbn,
   href,
+  showBookmark = true,
   linkComponent: LinkComponent,
 }: BookSearchResultProps) {
   const Content = (
     <div className="flex flex-row gap-[10px] cursor-pointer" onClick={() => onClickDetail?.(id)}>
-      <img
-        className="w-[100px] h-[140px] object-cover rounded-[10px]"
-        src={image}
-        alt={`${title} cover image`}
-        loading="lazy"
-      />
+      <img className="w-[100px] h-[140px] object-cover" src={image} alt={`${title} cover image`} loading="lazy" />
       <div className="flex flex-col w-full relative gap-[10px]">
+        {isbn && (
+          <div className="flex flex-row gap-2 items-center text-white">
+            <p className="typo-footnote">ISBN</p>
+            <p className="typo-title3">{isbn}</p>
+          </div>
+        )}
         <h3 className="typo-title3 text-white">{title}</h3>
         <p className="typo-footnote text-white">
           {author} • {publisher}
@@ -51,15 +57,17 @@ function BookSearchResult({
     <div className="flex w-full h-[140px] flex-row gap-[10px] relative">
       {LinkComponent && href ? <LinkComponent href={href}>{Content}</LinkComponent> : Content}
 
-      <button
-        className="absolute bottom-0.5 right-0.5"
-        onClick={(e) => {
-          e.stopPropagation();
-          onBookmarkToggle?.(id, !bookmark);
-        }}
-      >
-        <BookmarkSVG fill={bookmark ? color.primary : "none"} size="xl" />
-      </button>
+      {showBookmark && (
+        <button
+          className="absolute bottom-0.5 right-0.5"
+          onClick={(e) => {
+            e.stopPropagation();
+            onBookmarkToggle?.(id, !bookmark);
+          }}
+        >
+          <BookmarkSVG fill={bookmark ? color.primary : "none"} size="xl" />
+        </button>
+      )}
     </div>
   );
 }
