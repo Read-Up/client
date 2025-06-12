3f0ad05268686433c53f6a219a267fca459e6f0f
chanwoong1 <jcw7468@gmail.com>
2025-06-13 00:04:41 +0900

    chore: jest 테스트 환경 구성
    - apps/web, packages/ui에 jest 환경 설정
    - root에서 pnpm test를 위해 packages/utils의 packages.json test 스크립트
      제거

---
+++ apps/web/setupTests.ts
@@ -0,0 +1,16 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
+
+// prettier-ignore
+// beforeAll(() => {
+//   jest.spyOn(console, "error").mockImplementation(() => { });
+//   jest.spyOn(console, "warn").mockImplementation(() => { });
+// });
+
+// afterAll(() => {
+//   (console.error as jest.Mock).mockRestore();
+//   (console.warn as jest.Mock).mockRestore();
+// });
