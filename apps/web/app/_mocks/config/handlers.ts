6c009fb6c0c7a47ad615e0878b1385e1d4b92a3b
chanwoong1 <jcw7468@gmail.com>
2025-06-16 19:56:30 +0900

    chore: msw 오류 수정
    - apps/web에 msw 패키지 추가
    - 삭제된 mockup을 import하는 코드 제거

--- apps/web/app/_mocks/config/handlers.ts
+++ apps/web/app/_mocks/config/handlers.ts
@@ -1,6 +1,5 @@
 import { authMockups } from "../domain/auth";
 import { diaryMockups } from "../domain/diary";
 import { feedMockups } from "../domain/feed";
-import { memberMockups } from "../domain/member";
 
-export const handlers = [...authMockups, ...feedMockups, ...memberMockups, ...diaryMockups];
+export const handlers = [...authMockups, ...feedMockups, ...diaryMockups];
