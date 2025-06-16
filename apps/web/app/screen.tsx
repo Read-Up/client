046e26bbade9e169336e23434eb4b2d9f72379e3
JinMuGo <jingmuio@gmail.com>
2025-06-16 18:23:46 +0900

    fix: mock migration error

--- apps/web/app/screen.tsx
+++ apps/web/app/screen.tsx
@@ -1,6 +1,13 @@
-import Layout from "./_components/shared/layout";
+"use client";
 
+import Layout from "./_components/shared/layout";
+import initMocks from "./_mocks";
+import { useEffect } from "react";
 export default function HomeScreen() {
+  useEffect(() => {
+    initMocks();
+  }, []);
+
   return (
     // NOTE: 이후 PageLayout을 변경할 수 있기 때문에 HomeLayout에서 PageLayout을 사용하지 않고
     // screen.tsx에 PageLayout을 사용하도록 하였습니다.
