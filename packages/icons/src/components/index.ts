83fcb2c9c97bf795b000605ac72f79bb7303e7bb
chanwoong1 <jcw7468@gmail.com>
2025-06-23 01:30:22 +0900

    feature: save 아이콘 추가

--- packages/icons/src/components/index.ts
+++ packages/icons/src/components/index.ts
@@ -27,6 +27,7 @@ export { default as KeyLineSVG } from "./key-line.svg";
 export { default as KeywordLineSVG } from "./keyword-line.svg";
 export { default as MenuSVG } from "./menu.svg";
 export { default as PencilSVG } from "./pencil.svg";
+export { default as SaveSVG } from "./save.svg";
 export { default as ScanSVG } from "./scan.svg";
 export { default as SearchSVG } from "./search.svg";
 export { default as ShareFilledSVG } from "./share-filled.svg";
